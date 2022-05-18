d0c2090f9a2ff5207df2a9db248dce87307cdf79
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-18 18:56:53 -0400

    Update context.js

--- context.js
+++ context.js
@@ -2,6 +2,7 @@ const Route = ReactRouterDOM.Route;
 const Link = ReactRouterDOM.Link
 const HashRouter = ReactRouterDOM.HashRouter;
 const UserContext = React.createContext(null);
+const bankContext = React.createContext(null);
 let loggedin;
 
 function Card(props) {
