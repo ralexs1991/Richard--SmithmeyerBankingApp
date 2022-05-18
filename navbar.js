7a788a9c0dda260a4329f38f10ad1ec1d78326a9
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-18 19:17:57 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,127 +1,46 @@
-function NavBar(){
-    const ctx = React.useContext(UserContext);
-    const user = React.useContext(BankContext);
-   
-    const [currentUser, setCurrentUser] = React.useState();
-    React.useEffect(() => {
-      let user = ctx.users.filter((user) => user.log === true);
-      let index;
-      if (user.length > 0) {
-        index = ctx.users.indexOf(user[0]);
-        setCurrentUser(user[0]);
-      }
-    }, [ctx.users]);
-    function renderTooltip() { 
-    return <Tooltip>Tooltip for the Register button</Tooltip>;
-    } 
-    return (
-    <>
-      <Navbar bg='light' expand='lg'>
-        <Container>
-          <OverlayTrigger
-            placement='bottom'
-            overlay={
-              <Tooltip id={`tooltip-logo`}>
-                <strong>Fanky's</strong> Brand.
-              </Tooltip>
-            }>
-            <Navbar.Brand href='#/'>
-              <img
-                alt=''
-                src={img}
-                width='30'
-                height='30'
-                className='d-inline-block align-top'
-              />{' '}
-              Fanky
-            </Navbar.Brand>
-          </OverlayTrigger>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav defaultActiveKey='#/' fill variant='tabs'>
-              <Nav.Link href='#/'>
-                <OverlayTrigger
-                  placement='bottom'
-                  overlay={
-                    <Tooltip id={`tooltip-home`}>
-                      <strong>Fanky's</strong> welcome page.
-                    </Tooltip>
-                  }>
-                  <button className='btn btn-primary'>Home</button>
-                </OverlayTrigger>
-              </Nav.Link>
-              <Nav.Link href='#/CreateAccount/'>
-                <OverlayTrigger
-                  placement='bottom'
-                  overlay={
-                    <Tooltip id={`tooltip-createAcount`}>
-                      Register on <strong>Fanky's</strong>, your prefered
-                      insecure bank app.
-                    </Tooltip>
-                  }>
-                  <button className='btn btn-primary'>Create Account</button>
-                </OverlayTrigger>
-              </Nav.Link>
-              <Nav.Link href='#/deposit'>
-                <OverlayTrigger
-                  placement='bottom'
-                  overlay={
-                    <Tooltip id={`tooltip-deposit`}>
-                      <strong>Fanky's</strong> Deposit's page.
-                    </Tooltip>
-                  }>
-                  <button className='btn btn-primary'>Deposit</button>
-                </OverlayTrigger>
-              </Nav.Link>
-              <Nav.Link href='#/withdraw'>
-                <OverlayTrigger
-                  placement='bottom'
-                  overlay={
-                    <Tooltip id={`tooltip-withdraw`}>
-                      <strong>Fanky's</strong> Withdraw's page.
-                    </Tooltip>
-                  }>
-                  <button className='btn btn-primary'>Withdraw</button>
-                </OverlayTrigger>
-              </Nav.Link>
-              <Nav.Link href='#/balance/'>
-                <OverlayTrigger
-                  placement='bottom'
-                  overlay={
-                    <Tooltip id={`tooltip-balance`}>
-                      <strong>Fanky's</strong> Balance page.
-                    </Tooltip>
-                  }>
-                  <button className='btn btn-primary'>Balance</button>
-                </OverlayTrigger>
-              </Nav.Link>
-              <Nav.Link href='#/alldata/'>
-                <OverlayTrigger
-                  placement='bottom'
-                  overlay={
-                    <Tooltip id={`tooltip-allData`}>
-                      <strong>Fanky's</strong> Storaged Users Data.
-                    </Tooltip>
-                  }>
-                  <button className='btn btn-primary'>All Data</button>
-                </OverlayTrigger>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          <OverlayTrigger
-            placement='bottom'
-            overlay={
-              <Tooltip id={`tooltip-logIn`}>
-                <strong>Fanky's</strong> Log In page.
-              </Tooltip>
-            }>
-            <button className='btn btn-dark' onClick={logOut}>
-              {currentUser ? 'Log out' : 'Log In'}
-            </button>
-          </OverlayTrigger>
-        </Container>
-      </Navbar>
-    </>
-  );
+function tooltip () {
+    $('[data-toggle="tooltip"]').tooltip();
 }
 
+function NavBar () {
+    const [active, setActive] = React.useState('#/');
+    const setClass = (element) => (event) => {
+       let link = element.href;
+       setActive(link);
+    }
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid" >
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <ul className="navbar-nav">
+                    <li className='nav-item'>
+                        <a onClick={setClass({href:"#/"})} data-toggle="tooltip" data-placement="bottom" title="BadBank Home Page" className={"#/" === active ? 'nav-link active' : 'nav-link'} href="#/">BadBank</a>
+                    </li>
+                </ul>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a data-toggle="tooltip" data-placement="bottom" title="Create Account" className={"#/CreateAccount/" === active ? 'nav-link active' : 'nav-link'} onClick={setClass({href: "#/CreateAccount/"})} href='#/CreateAccount/'>Create Account</a>
+                        </li>
+                        <li className="nav-item">
+                            <a data-toggle="tooltip" data-placement="bottom" title="Login/Logout" className={"#/Login" === active ? 'nav-link active' : 'nav-link'} onClick={setClass({href:"#/Login"})} href='#/Login/'>Log In/Log Out</a>
+                        </li>
+                        {loggedin && <><li className="nav-item">
+                            <a data-toggle="tooltip" data-placement="bottom" title="Made a deposit" className={"#/deposit/" === active ? 'nav-link active' : 'nav-link'} onClick={setClass({ href: "#/deposit/" })} href='#/deposit/'>Deposit</a>
+                        </li>
+                        <li className="nav-item">
+                            <a data-toggle="tooltip" data-placement="bottom" title="Make a withdraw" className={"#/withdraw/" === active ? 'nav-link active' : 'nav-link'} onClick={setClass({ href: "#/withdraw/" })} href='#/withdraw/'>Withdraw</a>
+                        </li></>}
+
+                        <li className="nav-item">
+                            <a data-toggle="tooltip" data-placement="bottom" title="All Data" className={"#/alldata/" === active ? 'nav-link active' : 'nav-link'} onClick={setClass({href:"#/alldata/"})} href='#/alldata/'>All Data</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
