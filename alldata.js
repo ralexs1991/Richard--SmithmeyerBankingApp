ebf5d6c205fd30ef18f28f96a5ecc6049ad6a32e
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-19 10:17:45 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import Card from '../components/card';
-import {Table} from 'react-bootstrap';
-import {UserContext} from '../page';
-
-
 function AllData() {
   const ctx = React.useContext(UserContext);
   console.log(ctx);
