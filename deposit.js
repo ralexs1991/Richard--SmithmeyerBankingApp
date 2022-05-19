541edae6eafedf9686203501c44960b6123ba23e
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-19 14:36:12 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,53 +1,85 @@
-function Deposit(name, email, password, log,) {
-    const ctx = React.useContext(UserContext);
+function Withdrawl() { 
     const [show, setShow]         = React.useState(true);
     const [isValid, setIsValid]   = React.useState(false);
     const [status, setStatus]     = React.useState('');
     const [amount, setAmount]     = React.useState('');
     const [balance, setBalance] = React.useState('');
-     if (ctx.users) {
-            return true;
-        } else {            
-      alert('Please Login first');     
-    }        
-    console.log('user',);  
- 
+    const ctx = React.useContext(UserContext); 
   
-  function validate (field, value) {
-    console.log( ' type:' + typeof value);
-    console.log('String:' + value);
-  }
- (value !== '' && Number(value) !== 0 &&!NaN(value));{
-            setAmount(value);
-            setIsValid(true);
-        };
-     
-      
-    
-     function handleDeposit(){
-    if (!validate ( 'amount', amount, 1)) return;
-    if (ctx.users[0]) {
-      let newBalance = parseFloat(balance) + parseFloat(amount);
-      newBalance = newBalance.toFixed(2);
-      ctx.users[0].balance = Number(newBalance);
-      ctx[i].balance = parseFloat(balance).toFixed(2);
-  
+    let balance = 100;
+    if (ctx.users[0]){
+    balance= ctx.users[0].balance;
     }
+    balance = parseFloat(balance).toFixed(2);
+    
+    function validate(field, value){
+      function showError(errorMsg, showErrorMsg) {
+      if (showErrorMsg === 1){
+        setStatus('Error: ' + errorMsg);
+        setTimeout(() => setStatus(''), 3000);
+      }
+    }
+      
   
- 
-  }
+    let errorMsg = '';
   
+    if (field === 'amount'){
+      if(value === ''){
+          errorMsg = 'Enter Deposit amount';
+          setValid(false);
+          setAmount('');
+          showError(errorMsg, showErrorMsg);
+          return false;
+        } else if
+          (Number(value) <= 0) {
+            errorMsg = 'Invalid Amount: Must be positive number';
+            setAmount(value);
+            setIsValid(false);
+            showError(errorMsg, showErrorMsg);
+            return(false);}
+            else if (value !== '' && isNaN(value)){
+              errorMsg = 'Invalid Amount: Enter a positive number';
+              setValid(false);
+              showError(errorMsg, showErrorMsg, 1);
+              return false;
+            } else if (value !== '' && Number(value) !== 0 &&!isNaN(value)){
+              setAmount(value);
+              setIsValid(true);
+          }
+        }
+        return true;
+        }
   
-   return (
+        function handledeposit(){
+          if (!validate ( 'amount', amount, 1)) return;
+          if (ctx.users[0]) {
+            let newBalance = parseFloat(balance) + parseFloat(amount);
+            newBalance = newBalance.toFixed(2);
+          if (newBalance < 0) {
+            setStatus('Invalid Amount: Insufficient Funds');
+            setTimeout(() => setStatus(''), 3000);
+            clearForm();
+            return false;
+          }
+            ctx.users[0].balance = Number(newBalance);
+          } 
+          setShow(false);
+        }
+  
+        function clearForm(){
+          setAmount('');
+        }
+  
+        return (
           <>
-            <h1>Withdraw Funds</h1>
+            <h1>Deposit Funds</h1>
             <Card
               txtcolor= "black"
               header={`Current balance: $${balance}`}
               status= {status}
               body={show ? (
                 <>
-                  <label> Withdrawl Amount:</label><br/>
+                  <label> Deposit Amount:</label><br/>
                   $<input
                     type= "input"
                     id= "amount"
@@ -59,8 +91,8 @@ function Deposit(name, email, password, log,) {
                     type="submit"
                     className={`btn ${isValid ? "btn-primary": "btn-light text-black-50"}`}
                     id='submit-button'
-                    value={Withdrawl}
-                    onClick={handleWithdraw} />
+                    value={Deposit}
+                    onClick={handleDeposit} />
                   </>
               ):( 
                 <>
@@ -68,7 +100,7 @@ function Deposit(name, email, password, log,) {
                   <button
                     type='submit'
                     className="btn btn-primary"
-                    onClick={clearForm}> New withdraw 
+                    onClick={clearForm}> New Deposit 
                   </button>
                   </>
               )}
@@ -76,3 +108,4 @@ function Deposit(name, email, password, log,) {
           </>
         )
     }
+  
