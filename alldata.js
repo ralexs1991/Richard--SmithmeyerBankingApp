2413ed1a5774ac2201f378d356ce3b85710c62b8
ralexs1991 <94880853+ralexs1991@users.noreply.github.com>
2022-05-19 15:47:31 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,13 +1,13 @@
 function AllData() {
   const ctx = React.useContext(UserContext);
   
-  function getName() {
+  function getInfo() {
     const currentUser = ctx.users.filter((user) => user.loggedIn == true)
     if (currentUser.length > 0) {
       return currentUser[0].name;
     }
     else {
-      return "No one logged in"
+      return "Error: No users currently logged in."
     }
   }
 
@@ -19,7 +19,7 @@ function AllData() {
       body={
         <div>
           <div style={{paddingBottom: "5px"}}>
-            <h5 className="text-dark">Current User: {getName()}</h5>
+            <h5 className="text-dark">Current User: {getInfo()}</h5>
           </div>
           <Card/>
         </div>}
